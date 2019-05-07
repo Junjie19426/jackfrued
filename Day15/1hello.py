05c6a025cd891d834a8b109367aa409d3bc332c1
Junjie1955 <952195872@qq.com>
2019-05-07 11:07:29 +0800

    Initial commit

---
+++ Day15/1hello.py
@@ -0,0 +1,12 @@
+"""
+第一个Python程序 - hello，world！
+向伟大的Dennis M. Ritchie先生致敬
+
+
+Author:Junjie
+Date:2019-05-06
+
+"""
+print('hello, world!')
+print('hello', 'world', sep='$ ', end='!\n')   #sep=' ' :设置字符串之间的分隔符
+print('goodbye, world', end='!\n')   #end=‘ ’ ：设置的是行末的符号
---
+++ Day15/5leap.py
@@ -0,0 +1,11 @@
+"""
+输入年份 如果是闰年输出True 否则输出False
+
+Author：Junjie
+Date: 2019-05-06
+
+"""
+
+year = int(input('请输入年份：'))
+is_leap = (year % 4 == 0 and year % 100 !=0 or year % 400 ==0)
+print(is_leap)
